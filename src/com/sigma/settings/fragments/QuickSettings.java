6d3f122e2dc826d400a92d96c4d3a7fc37166d8d
Matt Filetto <matt.filetto@gmail.com>
2023-09-01 00:40:33 -0700

    QuickSettings: Add Page Transition animations [2/2]
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- src/com/sigma/settings/fragments/QuickSettings.java
+++ src/com/sigma/settings/fragments/QuickSettings.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2023 Evolution X
+ * Copyright (C) 2023 SigmaDroid
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -61,12 +62,14 @@ public class QuickSettings extends DashboardFragment implements
     private static final String KEY_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
     private static final String QUICK_PULLDOWN = "status_bar_quick_qs_pulldown";
     private static final String overlayThemeTarget  = "com.android.systemui";
+    private static final String QS_PAGE_TRANSITIONS = "custom_transitions_page_tile";
 
     private ListPreference mShowBrightnessSlider;
     private ListPreference mBrightnessSliderPosition;
     private ListPreference mQuickPulldown;
     private SwitchPreference mShowAutoBrightness;
     private SystemSettingEditTextPreference mFooterString;
+    private SystemSettingListPreference mPageTransitions;
     private SystemSettingListPreference mQsStyle;
     private ThemeUtils mThemeUtils;
     private Handler mHandler;
@@ -85,6 +88,14 @@ public class QuickSettings extends DashboardFragment implements
         final Context mContext = getActivity().getApplicationContext();
         final ContentResolver resolver = mContext.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mPageTransitions = (SystemSettingListPreference) findPreference(QS_PAGE_TRANSITIONS);
+        mPageTransitions.setOnPreferenceChangeListener(this);
+        int customTransitions = Settings.System.getIntForUser(resolver,
+                Settings.System.CUSTOM_TRANSITIONS_KEY,
+                0, UserHandle.USER_CURRENT);
+        mPageTransitions.setValue(String.valueOf(customTransitions));
+        mPageTransitions.setSummary(mPageTransitions.getEntry());
 
         mQsStyle = (SystemSettingListPreference) findPreference(KEY_QS_PANEL_STYLE);
         mCustomSettingsObserver.observe();
@@ -157,6 +168,14 @@ public class QuickSettings extends DashboardFragment implements
             return true;
         } else if (preference == mQsStyle) {
             mCustomSettingsObserver.observe();
+            return true;
+        } else if (preference == mPageTransitions) {
+            int customTransitions = Integer.parseInt(((String) newValue).toString());
+            Settings.System.putIntForUser(getContentResolver(),
+                    Settings.System.CUSTOM_TRANSITIONS_KEY, customTransitions, UserHandle.USER_CURRENT);
+            int index = mPageTransitions.findIndexOfValue((String) newValue);
+            mPageTransitions.setSummary(
+                    mPageTransitions.getEntries()[index]);
             return true;
         }
         return false;
